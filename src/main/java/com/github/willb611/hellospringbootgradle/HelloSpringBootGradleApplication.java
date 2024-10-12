3e353026de47a77a1071c6dbdb046b463e8f409d
William A. O. Brown <will.b611@gmail.com>
2024-10-12 16:24:02 +0100

    Init project

---
+++ src/main/java/com/github/willb611/hellospringbootgradle/HelloSpringBootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.github.willb611.hellospringbootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootGradleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloSpringBootGradleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/willb611/hellospringbootgradle/HelloSpringBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.willb611.hellospringbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootGradleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
